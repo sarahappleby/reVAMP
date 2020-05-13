610108c25bac931a37df2dbafcae749eadd9cf99
Sarah Appleby <sarahappleby20@gmail.com>
2020-05-13 10:31:48 +0100

    generate fake data in optical depths

--- workspace/runners/multinest_params/generate_test_spectra.py
+++ workspace/runners/multinest_params/generate_test_spectra.py
@@ -1,5 +1,6 @@
 import autofit as af
 import os
+import numpy as np
 import sys
 sys.path.append('/home/sarah/reVAMP/')
 import matplotlib.pyplot as plt
@@ -14,11 +15,11 @@ config_path = workspace_path + "config"
 af.conf.instance = af.conf.Config(
     config_path=config_path, output_path=workspace_path + "output")
 
-spectrum_dir = '/home/sarah/VAMP/vamp_2.0/vamp_workspace/runners/multinest_params/data/'
+spectrum_dir = '/home/sarah/reVAMP/workspace/runners/multinest_params/data/'
 
 centers_a = [1., 1., 2., -2., 0.]
-centers_b = [0., -2., -1., 3., -3.]
-centers_c = [-2., 3., -3., 0., 2.]
+centers_b = [0., -2., -1., 1., -1.]
+centers_c = [-2., 3., -3., 0.5, 2.]
 sigmas_a = [1.0, 1.0, 2.0, 0.5, 1.0]
 sigmas_b = [0.5, 2.0, 0.5, 0.5, 1.5]
 sigmas_c = [2.0, 3.0, 1.0, 0.5, 2.0]
@@ -28,18 +29,6 @@ intensity_c = [0.3, 0.5, 0.7, 0.6, 0.7]
 
 combos = ['a', 'b', 'c', 'd', 'e']
 
-centers_a = [0.]
-centers_b = [-3.]
-centers_c = [2.]
-sigmas_a = [1.0]
-sigmas_b = [1.5]
-sigmas_c = [2.0]
-intensity_a = [0.4]
-intensity_b = [1.0]
-intensity_c = [0.7]
-
-combos = ['e']
-
 for i, combo in enumerate(combos):
 
 	attrs = {}
@@ -47,17 +36,17 @@ for i, combo in enumerate(combos):
 	fakeGaussB = FakeGauss(center=centers_b[i], sigma=sigmas_b[i], intensity=intensity_b[i])
 	fakeGaussC = FakeGauss(center=centers_c[i], sigma=sigmas_c[i], intensity=intensity_c[i])
 
-	dataset_a = Spectrum(fakeGaussA.x, fakeGaussA.x, 1.0 - fakeGaussA.gauss - fakeGaussA.noise, fakeGaussA.noise)
+	dataset_a = Spectrum(fakeGaussA.x, fakeGaussA.x, np.exp(-1. * fakeGaussA.gauss) + fakeGaussA.noise, fakeGaussA.noise)
 	attrs['center_0'] = centers_a[i]; attrs['sigma_0'] = sigmas_a[i]; attrs['intensity_0'] = intensity_a[i]
 	dataset_a.save_as_h5py(spectrum_dir+'combo_' + combo + '_1_component.h5', attributes=attrs)
 	dataset_a.plot_spectrum(spectrum_dir+'combo_' + combo + '_1_component.png')
 	
-	dataset_b = Spectrum(fakeGaussB.x, fakeGaussB.x, 1.0 - fakeGaussA.gauss - fakeGaussB.gauss - fakeGaussB.noise, fakeGaussB.noise)
+	dataset_b = Spectrum(fakeGaussB.x, fakeGaussB.x, np.exp(-1. * (fakeGaussA.gauss + fakeGaussB.gauss)) + fakeGaussB.noise, fakeGaussB.noise)
 	attrs['center_1'] = centers_b[i]; attrs['sigma_1'] = sigmas_b[i]; attrs['intensity_1'] = intensity_b[i]
 	dataset_b.save_as_h5py(spectrum_dir+'combo_' + combo + '_2_component.h5', attributes=attrs)
 	dataset_b.plot_spectrum(spectrum_dir+'combo_' + combo + '_2_component.png')
 
-	dataset_c = Spectrum(fakeGaussC.x, fakeGaussC.x, 1.0 - fakeGaussA.gauss - fakeGaussB.gauss - fakeGaussC.gauss - fakeGaussC.noise, fakeGaussC.noise)
+	dataset_c = Spectrum(fakeGaussC.x, fakeGaussC.x, np.exp(-1. * (fakeGaussA.gauss + fakeGaussB.gauss + fakeGaussC.gauss)) + fakeGaussC.noise, fakeGaussC.noise)
 	attrs['center_2'] = centers_c[i]; attrs['sigma_2'] = sigmas_c[i]; attrs['intensity_2'] = intensity_c[i]
 	dataset_c.save_as_h5py(spectrum_dir+'combo_' + combo + '_3_component.h5', attributes=attrs)
 	dataset_c.plot_spectrum(spectrum_dir+'combo_' + combo + '_3_component.png')
